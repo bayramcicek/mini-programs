b54fa10096829a0609f7ef782fcdf2b908e2d54f
bayramcicek <mail@bayramcicek.com.tr>
2018-07-11 21:09:47 +0300

    find ramanujan numbers

---
+++ p004_ramanujan_number.py
@@ -0,0 +1,18 @@
+# 1729
+# 1+7+2+9= 19
+# 19 * 91 = 1729
+
+ramanujan = []
+for n in range(1000,10000): # you can change the range whatever you want
+    top = 0
+    for i in list(str(n)):
+        top +=int(i)
+    if(top<10):
+        continue
+
+    # reverse number (exp: 19 --> 91)
+    reverseTop = int( list(str(top))[-1] + list(str(top))[-2] )
+    if(top*reverseTop == n):
+        ramanujan.append(n)
+
+print(ramanujan)
