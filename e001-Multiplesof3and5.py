2d68a2177f82688bad88efdd3c1e50e7ed21d2e9
bayramcicek <mail@bayramcicek.com.tr>
2018-10-06 20:23:29 +0300

    multiples 3 5

---
+++ e001-Multiplesof3and5.py
@@ -0,0 +1,15 @@
+three,five,fiveteen= 3,5,15 
+sum=0
+while(three<1000):
+    sum +=three
+    three +=3
+
+while(five<1000):
+    sum +=five
+    five +=5
+
+while(fiveteen<1000):
+    sum -=fiveteen
+    fiveteen +=15
+
+print(sum)
