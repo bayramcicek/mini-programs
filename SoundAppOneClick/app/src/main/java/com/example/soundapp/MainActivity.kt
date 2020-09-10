3df203d46c4bcf689271538da89d57b85612d207
bayramcicek <mail@bayramcicek.com.tr>
2020-09-10 14:46:43 +0300

    show amplitude app

---
+++ SoundAppOneClick/app/src/main/java/com/example/soundapp/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.soundapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    private var timer: Timer? = null
+    private var recorder: MediaRecorder? = null
+
+    private lateinit var startButton: Button
+    private var isRecording: Boolean = false
+
+    // Requesting permission to RECORD_AUDIO
+    private val requiredPermissions: Array<String> = arrayOf(
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    // onCreate
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.startRecording)
+    } // onCreate
+
+    fun startRecording(view: View) {
+        if (!permissionsIsGranted(requiredPermissions)) {
+            ActivityCompat.requestPermissions(this, requiredPermissions, 200)
+            return
+        }
+
+        if (!isRecording) {
+            isRecording = true
+            startButton.text = "STOP_REC"
+
+            //Creating MediaRecorder and specifying audio source, output format, encoder & output format
+            recorder = MediaRecorder().apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP) // it: MPEG_4
+                setAudioEncoder(MediaRecorder.AudioEncoder.AAC) // me: AMR_NB
+                setOutputFile("/dev/null")
+                setAudioSamplingRate(48000)
+                setAudioEncodingBitRate(48000)
+
+                try {
+                    prepare()
+                    Log.i("startRecording", "startRecording")
+                } catch (e: IOException) {
+                    Log.e("Prepare", "prepare() failed")
+                }
+
+                start()
+            }
+            calculateAmp()
+
+        } else {
+            isRecording = false
+            startButton.text = "START_REC"
+            timer?.cancel()
+
+            Log.i("stopRecording", "stopRecording")
+            amp_tv.text = "0"
+
+            recorder?.apply {
+                stop()
+                reset()
+                release()
+            }
+            recorder = null
+        }
+    }
+
+    private fun calculateAmp() {
+        timer = Timer()
+        timer?.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    if (recorder != null) {
+                        val currentMaxAmplitude = recorder?.maxAmplitude // 0-32767
+                        amp_tv.text = currentMaxAmplitude.toString()
+                    }
+                }
+            }
+        }, 0, 200)
+    }
+
+    private fun permissionsIsGranted(perms: Array<String>): Boolean {
+        for (perm in perms) {
+            val checkVal: Int = checkCallingOrSelfPermission(perm)
+            if (checkVal != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        for (result in grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+        }
+        startRecording(View(this))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("onPause", "onPause")
+        cancelAll()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onStop", "onStop")
+        cancelAll()
+    }
+
+    private fun cancelAll() {
+        timer?.cancel()
+
+        recorder?.apply {
+            stop()
+            reset()
+            release()
+        }
+        recorder = null
+
+        if (isRecording) {
+            isRecording = false
+            startButton.text = "START_REC"
+        }
+        amp_tv.text = "0"
+    }
+}
